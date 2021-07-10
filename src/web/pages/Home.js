80ad87e1052bd2a09cdb669a952d6cee349384bb
LukaSesar <luka.sesar99@gmail.com>
2021-07-10 15:58:27 +0200

    spinning coin added

--- src/web/pages/Home.js
+++ src/web/pages/Home.js
@@ -2,7 +2,14 @@ import React from "react";
 import "../style.scss";
 
 const Home = () => {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="coin">
+        <div class="front"></div>
+        <div class="back"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
